a1a96229ed4c237cf110a097b25f0e84c37870fd
Zephyrior <mark.temblique@gmail.com>
2025-04-11 12:19:38 +0200

    creazione prima query & cancellazione faker

--- src/main/java/it/epicode/ProgettoSettimanaleS17L5/postazioni/PostazioneRepository.java
+++ src/main/java/it/epicode/ProgettoSettimanaleS17L5/postazioni/PostazioneRepository.java
@@ -1,6 +1,13 @@
 package it.epicode.ProgettoSettimanaleS17L5.postazioni;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostazioneRepository extends JpaRepository<Postazione, Long> {
+
+    @Query("SELECT p FROM Postazione p WHERE p.tipologia = :tipologia AND p.edificio.citta = :citta")
+    List<Postazione> findByTipologiaAndCitta(@Param("tipologia") TipoPostazione tipologia, @Param("citta") String citta);
 }
